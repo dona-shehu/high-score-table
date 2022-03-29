1581e3f4b3976d25a8bec261b07a7099e644ce51
dona-shehu <shehu.donna@gmail.com>
2022-03-29 11:42:41 +0100

    Level 2 & 3 completed

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -6,27 +6,32 @@ const HighScoreTable = (props) => {
     return (
         <>
             <header>
-                <h1>High Scores Per Countries </h1>
+                <h1>High Scores Per County </h1>
             </header>
-            {props.data.map((el,index) => {
+            <div className='table-container'>
+            {props.data.sort((a,b)=> a.name.localeCompare(b.name))
+            .map((el,index) => {
                 return (
-                    <table key={index}>
+                    <div className='card'>
+                    <table className='table' key={index}>
                         <tr>
                             <th>High Scores: {el.name} </th>
                         </tr>
-                       
-                            {el.scores.map((player, index) => {
+                            <tbody>
+                            {el.scores.sort((a,b)=>a.s < b.s ? 1: -1).map((player, index) => {
                                 return (
                                     <tr key={index}>
                                         <td>{player.n.toUpperCase()}</td>
-                                        <td>{player.s}</td>
+                                        <td className='numbers'>{parseInt(player.s).toLocaleString()}</td>
                                     </tr>
                                 )
                             })}
-                        
+                          </tbody >
                     </table>
+                    </div>
                 )
             })}
+            </div>
         </>
     );
     
